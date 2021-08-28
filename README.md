298425cad0a458042752f95526deb030b82aead1
Daniel Barnes <81057377+danielzbarnes@users.noreply.github.com>
2021-08-27 17:52:28 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # NetworkMonitorApp
- A simple app to display the current internet connection status.
+A simple app to display the current internet connection status by observing a LiveData object.  Jetpack Compose was added as an afterthought and an excercise in manually adding Compose dependencies to the gradle.
--- app/src/main/java/com/danielzbarnes/networkmonitorapp/MainActivityViewModel.kt
+++ app/src/main/java/com/danielzbarnes/networkmonitorapp/MainActivityViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.danielzbarnes.networkmonitorapp.network.NetworkLiveData
 
+// this ViewModel is largely unnecessary is only here for testing testing behavior
 class MainActivityViewModel(): ViewModel() {
 
     fun getNetworkLiveData() = NetworkLiveData
